14ab8dc0690d53c5ed5acda921c6d160e2b37a94
juank8911 <juancarlos_891111@hotmail.com>
2021-11-30 13:44:27 -0500

    cambio de puerto

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     domain: 'localhost',
     // domain:'192.168.2.102',
     // domain:'localhost',
-    puerto: process.env.PORT || 3000,
+    puerto: process.env.PORT || 3300,
     // nombredb: 'prevenirexpres',
     // host: 'localhost',
     // host: '157.230.49.162',
