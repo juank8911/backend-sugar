f7ce8d11619f9053c12eca3b272e5e75d0091889
juank8911 <juancarlos_891111@hotmail.com>
2021-12-17 11:54:02 -0500

    activa perf

--- controler/userDao.js
+++ controler/userDao.js
@@ -1,222 +1,296 @@
-const conection = require('../controler/connection');
-const userModel = require('../schemas/UserSchema');
-const MemModel = require('../schemas/memberSchema');
-const perfModel = require('../schemas/perfilSchema');
-const email = require('../controler/email');
-const config = require('../config');
-const jwt = require('jsonwebtoken');
-
-
-
+const conection = require("../controler/connection");
+const userModel = require("../schemas/UserSchema");
+const MemModel = require("../schemas/memberSchema");
+const perfModel = require("../schemas/perfilSchema");
+const email = require("../controler/email");
+const config = require("../config");
+const jwt = require("jsonwebtoken");
 
 let UserDao = {};
 
 UserDao.registroMember = (register, callback) => {
-
     const newUser = new userModel(register.newUsr);
     const newMemb = new MemModel(register.newMemb);
     console.log("registrMember Dao");
-    console.log('dentro de register dao')
+    console.log("dentro de register dao");
     try {
         newMemb.save(function(err) {
-
             if (err) {
                 console.log("Error save");
-                callback(null, { 'err': err, 'res': 'false' });
+                callback(null, { err: err, res: "false" });
             }
 
-            console.log('usuario registrado con exito.');
+            console.log("usuario registrado con exito.");
             newUser.save(function(err) {
                 if (err) {
-                    MemModel.findOneAndDelete({ 'identification': newMemb.identification }, (errfn, data) => {
-                        if (errfn) callback(null, { 'err': err, 'res': 'false' });
-                        if (data) callback(null, { 'data': data, 'res': 'true' });
+                    console.log(err);
+                    MemModel.findOneAndDelete({ identification: newMemb.identification },
+                        (errfn, data) => {
+                            if (errfn) callback(null, { err: err, res: "false" });
+                            if (data) throw err;
+                        }
+                    );
+                } else {
+                    console.log(" member registrado con exito.");
+                    console.log(newUser.salt);
+                    var usu = {
+                        to: newUser.email,
+                        salt: newUser.salt,
+                        texto: "Bienvenidos",
+                    };
+                    email.sendMail(usu, (err, ressp) => {
+                        console.log("correo enciado" + err);
+                        console.log("correo enciado" + ressp);
+                        if (err) {
+                            console.log("send mail fail");
+                            throw err;
+                        } else if (ressp) {
+                            console.log("send mail ok");
+                            // var token = jwt.sign(newUser, config.jwt_secreto);
+                            // console.log(token);
+                            callback(null, ressp);
+                        }
                     });
-
                 }
-
-                console.log(' member registrado con exito.');
-                console.log(newUser.salt);
-                var usu = {
-                    to: newUser.email,
-                    salt: newUser.salt,
-                    texto: 'Bienvenidos'
-                }
-                email.sendMail(usu, (err, ressp) => {
-                    console.log('correo enciado' + err)
-                    console.log('correo enciado' + ressp)
-                    if (err) {
-                        console.log('send mail fail')
-                        throw err
-                    } else if (ressp) {
-                        console.log('send mail ok');
-                        // var token = jwt.sign(newUser, config.jwt_secreto);
-                        // console.log(token);
-                        callback(null, ressp)
-                    }
-                });
-
             });
         });
     } catch (error) {
-        console.log(error)
-        console.log('return false')
-        callback(null, { 'res': 'false' })
+        console.log(error);
+        console.log("return false");
+        callback(null, { res: "false" });
     }
+};
 
-}
+UserDao.userUp = async(updt, callback) => {
+    console.log("actualiza usuario");
+    console.log(updt);
+    var usuaup = updt.usuup;
+    var membeup = updt.membup;
+    var filter = { member: usuaup.id };
 
-UserDao.userUp = (usuUp, callback) => {
-    userModel.findOneAndUpdate(usup, (err, user) => {
-        if (err) throw err;
-        if (user) {
-            console.log(user);
-            callback(null, user);
-        }
-    })
-}
+    let doc = await userModel.findOneAndUpdate(filter, usuaup);
+    console.log(doc);
+    // userModel.findOneAndUpdate({ usuaup }, (errus, user) => {
+    //     if (errus) throw err;
+    //     else {
+    //         console.log(user);
+    //         console.log("actualiza member");
+    //         console.log(memup);
+    //         MemModel.findOneAndUpdate(membeup, (errmem, memb) => {
+    //             if (errmem) throw errmem;
+    //             else {
+    //                 if (memb == nul || memb == "null") {
+    //                     callback(null, { upd: false, res: "member dont found" });
+    //                 } else {
+    //                     callback(null, { upd: true, res: "member update" });
+    //                 }
+    //             }
+    //         });
+    //     }
+    // });
+};
 
 UserDao.activa = (act, callback) => {
-    console.log('activa perfil');
-    console.log(act)
-    userModel.findOne({ 'email': act.email }, function(err, usua) {
+    console.log("activa perfil");
+    console.log(act);
+    userModel.findOne({ email: act.email, salt: act.salt }, function(err, usua) {
         console.log(usua);
-        if (err) { throw err } else {
-            if (usua == null || usua == 'null') {
-                console.log('usuario no encontrado')
-                callback(null, { 'usuario': null, 'activate': false })
+        if (err) {
+            console.log("error");
+            throw err;
+        } else {
+            if (usua == null || usua == "null") {
+                console.log("usuario no encontrado");
+                callback(null, { usuario: null, activate: false });
             }
             usua.valSession = 1;
-            userModel.updateOne(usua, (err, respo) => {
-                console.log(respo)
-
-                if (err) { throw err }
-                if (respo) {
-                    console.log('usuario actualizado')
+            $set = { valSession: 1 };
+            userModel
+                .updateOne({ id: usua.id }, // <-- find stage
+                    {
+                        $set,
+                    }
+                )
+                .then((result) => {
+                    console.log("usuario actualizado");
                     var lock = false;
-                    console.log(usua.member)
-                    MemModel.findOne({ 'identification': usua.member }, (err, memb) => {
-                        console.log('memModel find')
-                        console.log(memb)
-                        if (err) { throw err }
-                        if (memb) {
-                            var user = { usu: usua.email, usernam: usua.namUsu, name: memb.name, last: memb.last, member: usua.member, 'loked': 'unlock' }
-                            var token = jwt.sign(user, config.jwt_secreto);
-                            callback(null, { 'res': true, 'token': token })
+                    console.log(usua.member);
+                    MemModel.findOne({ identification: usua.member }, (err, memb) => {
+                        console.log("memModel find");
+                        console.log(memb);
+                        if (err) {
+                            throw err;
                         }
-                    })
+                        if (memb) {
+                            var user = {
+                                usu: usua.email,
+                                usernam: usua.namUsu,
+                                name: memb.name,
+                                last: memb.last,
+                                member: usua.member,
+                                loked: "unlock",
+                            };
+                            var token = jwt.sign(user, config.jwt_secreto);
+                            callback(null, { res: true, token: token });
+                        }
+                    });
 
+                    // userModel.updateOne(usua.id,
+                    //     (err, respo) => {
+                    //     console.log(respo);
 
-
-                }
-            })
+                    //     if (err) {
+                    //         console.log("err update");
+                    //         throw err;
+                    //     }
+                    //     if (respo) {
+                    //         console.log("usuario actualizado");
+                    //         var lock = false;
+                    //         console.log(usua.member);
+                    //         MemModel.findOne({ identification: usua.member }, (err, memb) => {
+                    //             console.log("memModel find");
+                    //             console.log(memb);
+                    //             if (err) {
+                    //                 throw err;
+                    //             }
+                    //             if (memb) {
+                    //                 var user = {
+                    //                     usu: usua.email,
+                    //                     usernam: usua.namUsu,
+                    //                     name: memb.name,
+                    //                     last: memb.last,
+                    //                     member: usua.member,
+                    //                     loked: "unlock",
+                    //                 };
+                    //                 var token = jwt.sign(user, config.jwt_secreto);
+                    //                 callback(null, { res: true, token: token });
+                    //             }
+                    //         });
+                    //     }
+                    // });
+                });
         }
-    })
-}
+    });
+};
 
 UserDao.validateRegistro = (valide, callback) => {
-    console.log('valida')
-    var resp = { 'email': false, 'phone': false, 'namUsu': false, 'identification': false };
-    userModel.findOne({ 'email': valide.email }, function(err, user) {
-
+    console.log("valida");
+    var resp = {
+        email: false,
+        phone: false,
+        namUsu: false,
+        identification: false,
+    };
+    userModel.findOne({ email: valide.email }, function(err, user) {
         console.log(user);
         if (err) throw err;
         else if (user == null) {
             resp.email = true;
         }
-        userModel.findOne({ 'phone': valide.phone }, function(err, userMov) {
+
+        userModel.findOne({ phone: valide.phone }, function(err, userMov) {
             console.log(userMov);
             if (err) throw err;
             else if (userMov == null) resp.phone = true;
-            userModel.findOne({ 'namUsu': valide.namUsu }, function(err, userNam) {
+            userModel.findOne({ namUsu: valide.namUsu }, function(err, userNam) {
                 console.log(userNam);
                 if (err) throw err;
                 else if (userNam == null) resp.namUsu = true;
-                MemModel.findOne({ 'identification': valide.identification }, (err, memCed) => {
-                    if (err) throw err;
-                    else if (memCed == null) resp.identification = true;
-                    callback(null, resp);
-                })
-
+                MemModel.findOne({ identification: valide.identification },
+                    (err, memCed) => {
+                        if (err) throw err;
+                        else if (memCed == null) resp.identification = true;
+                        callback(null, resp);
+                    }
+                );
             });
-
-        })
-
-
+        });
     });
 };
 
 UserDao.login = (login, callback) => {
-    console.log('login')
+    console.log("login");
     console.log(login);
 
-    userModel.findOne({ 'email': login.email, 'password': login.pssw }, function(err, user) {
-        console.log('error ' + err);
-        console.log('user ' + user);
-        if (err) {
-            console.log(err);
-            callback(null, err)
-        } else {
-            // console.log(err);
-            console.log('%s %s is a %s.', user);
-
-            if (user == null || user == 'null') {
-                console.log('usuario null')
-                callback(null, { 'res': false, 'token': null })
+    userModel.findOne({ email: login.email, password: login.pssw },
+        function(err, user) {
+            console.log("error " + err);
+            console.log("user " + user);
+            if (err) {
+                console.log(err);
+                callback(null, err);
             } else {
-                if (user.valSession == 1 || user.valSession == 1) {
-                    console.log('usuario find')
-                    var lock = false;
-                    MemModel.findOne({ 'identification': usua.member }, (err, memb) => {
-                        if (err) { throw err }
-                        if (memb) {
-                            var user = { usu: usua.email, usernam: usua.namUsu, name: memb.name, last: memb.last, member: usua.member, 'loked': lock }
-                            var token = jwt.sign(user, config.jwt_secreto);
-                            callback(null, { 'res': true, 'token': token })
-                        }
-                    })
+                // console.log(err);
+                console.log("%s %s is a %s.", user);
+
+                if (user == null || user == "null") {
+                    console.log("usuario null");
+                    callback(null, { res: false, token: null });
                 } else {
-                    lock = true;
-                    var usu = { user: user.email, usernam: user.namUsu, member: user.member, 'loked': unlock }
-                    var token = jwt.sign(usu, config.jwt_secreto);
-                    callback(null, { 'res': true, 'token': token })
+                    if (user.valSession == 1 || user.valSession == 1) {
+                        console.log("usuario find");
+                        var lock = false;
+                        MemModel.findOne({ identification: usua.member }, (err, memb) => {
+                            if (err) {
+                                throw err;
+                            }
+                            if (memb) {
+                                var user = {
+                                    usu: usua.email,
+                                    usernam: usua.namUsu,
+                                    name: memb.name,
+                                    last: memb.last,
+                                    member: usua.member,
+                                    loked: lock,
+                                };
+                                var token = jwt.sign(user, config.jwt_secreto);
+                                callback(null, { res: true, token: token });
+                            }
+                        });
+                    } else {
+                        lock = true;
+                        var usu = {
+                            user: user.email,
+                            usernam: user.namUsu,
+                            member: user.member,
+                            loked: unlock,
+                        };
+                        var token = jwt.sign(usu, config.jwt_secreto);
+                        callback(null, { res: true, token: token });
+                    }
                 }
             }
         }
-
-    })
-}
+    );
+};
 
 UserDao.updatePerfil = (perfil, callback) => {
-
-    console.log('metoo de update perfil');
+    console.log("metoo de update perfil");
     callback(null, perfil);
     updtPerf = new perfModel(register.updtPerf);
-    if (updtPerf.findOne({ 'identficacion': perfil.identification })) {
-
-    } else {
+    if (updtPerf.findOne({ identficacion: perfil.identification })) {} else {
         updtPerf.save();
     }
 };
 
-
 UserDao.validaAdmin = (req, res, next) => {
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
-    console.log('valida Admin');
+    var token =
+        req.body.token || req.query.token || req.headers["x-access-token"];
+    console.log("valida Admin");
     console.log(token);
     if (token) {
         jwt.verify(token, config.jwt_secreto, (err, decoded) => {
             if (err) {
                 // console.log(err);
-                return res.status(403).send({ 'mensaje': 'error al validar usuario, inicie sesion de nuevo' });
+                return res.status(403).send({
+                    mensaje: "error al validar usuario, inicie sesion de nuevo",
+                });
             }
             req.decoded = decoded;
-            console.log(decoded)
+            console.log(decoded);
             next();
         });
     }
-}
+};
 
-
-
-module.exports = UserDao
+module.exports = UserDao;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,13 @@
-const userDao = require('../controler/userDao');
-const newUs = require('../schemas/UserSchema');
-const newMem = require('../schemas/memberSchema');
-const ciclo = require('../controler/ciclos');
-const mongoose = require('mongoose');
-const { callbackPromise } = require('nodemailer/lib/shared');
+const userDao = require("../controler/userDao");
+const newUs = require("../schemas/UserSchema");
+const newMem = require("../schemas/memberSchema");
+const ciclo = require("../controler/ciclos");
+const mongoose = require("mongoose");
+const { callbackPromise } = require("nodemailer/lib/shared");
 
 module.exports = function(app) {
-
-    app.post('/register', (req, response) => {
-        console.log('dentro de register');
+    app.post("/register", (req, response) => {
+        console.log("dentro de register");
         var cod;
         newUs.init();
         newMem.init();
@@ -18,34 +17,32 @@ module.exports = function(app) {
             name: req.body.name,
             lastName: req.body.lastName,
             bornDate: req.body.bornDate,
-        })
+        });
         console.log(newMemb);
         ciclo.generaSalt((err, gen) => {
             cod = gen;
         });
-        newUsr = new newUs({
-                email: req.body.email,
-                password: req.body.pssw,
-                phone: req.body.phone,
-                face: req.body.face,
-                salt: cod,
-                namUsu: req.body.namUsu,
-                valSession: 0,
-                idFacebook: "abc",
-                idGmail: "def",
-                member: newMemb.identification
-            }),
-            console.log(newUsr);
+        (newUsr = new newUs({
+            email: req.body.email,
+            password: req.body.pssw,
+            phone: req.body.phone,
+            face: req.body.face,
+            salt: cod,
+            namUsu: req.body.namUsu,
+            valSession: 0,
+            idFacebook: "abc",
+            idGmail: "def",
+            member: newMemb.identification,
+        })),
+        console.log(newUsr);
         console.log("registr");
         userDao.registroMember({ newUsr, newMemb }, (err, data) => {
             if (err) response.status(400).send(err);
             if (data) response.status(200).send(data);
-        })
-
-
+        });
     });
 
-    app.post('/validatereg', async(req, resp) => {
+    app.post("/validatereg", async(req, resp) => {
         var valide = {
             email: req.body.email,
             phone: req.body.phone,
@@ -58,10 +55,10 @@ module.exports = function(app) {
         });
     });
 
-    app.post('/login', (req, resp) => {
+    app.post("/login", (req, resp) => {
         var login = {
             email: req.body.email,
-            pssw: req.body.pssw
+            pssw: req.body.pssw,
         };
 
         userDao.login(login, (err, data) => {
@@ -70,44 +67,53 @@ module.exports = function(app) {
         });
     });
 
-    app.post('/userup', (req, resp) => {
-        var usu = {
-            'id': req.body.id,
-            'namUsu': req.body.namUsu,
-            'email': req.body.email,
-            'phone': req.body.phone,
-            'name': req.body.name,
-            'last': req.body.last,
-            'bornDate': req.body.date
-        }
-        userDao.userUp(usu, (err, res) => {
-            if (err) { resp.status(400).send(err) }
-            if (res) { resp.status(200).send(res) }
-        })
-    })
+    app.put("/userup", userDao.validaAdmin, async(req, resp) => {
+        var usuup = {
+            id: req.body.id,
+            namUsu: req.body.namUsu,
+            email: req.body.email,
+            phone: req.body.phone,
+        };
+        var membup = {
+            identification: req.body.id,
+            name: req.body.name,
+            last: req.body.last,
+            bornDate: req.body.date,
+        };
+        var updt = { usuup: usuup, membup: membup };
+        await userDao.userUp(updt, (err, res) => {
+            if (err) {
+                resp.status(400).send(err);
+            }
+            if (res) {
+                resp.status(200).send(res);
+            }
+        });
+    });
 
-    app.post('/activaperf', (req, resp) => {
+    app.post("/activaperf", (req, resp) => {
         var act = {
-            'email': req.body.email,
-            'salt': req.body.salt
-        }
+            email: req.body.email,
+            salt: req.body.salt,
+        };
         userDao.activa(act, (err, res) => {
-            if (err) { resp.status(500).send(error); }
-            if (res) { resp.status(200).send(res) }
-        })
+            if (err) {
+                resp.status(500).send(error);
+            }
+            if (res) {
+                resp.status(200).send(res);
+            }
+        });
+    });
 
-    })
-
-    app.post('/perfilup', userDao.validaAdmin, (req, resp) => {
+    app.post("/perfilup", userDao.validaAdmin, (req, resp) => {
         var perfil = {
             identification: req.body.id,
             altura: req.body.altura,
-        }
-        console.log('dentro de perfil')
+        };
+        console.log("dentro de perfil");
         userDao.updatePerfil(perfil, (err, repUp) => {
-            resp.status(200).send('ok')
-        })
-
-
+            resp.status(200).send("ok");
+        });
     });
-}
+};
